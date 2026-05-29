524ab6fcc43ddda631b4ea6cbd16cb4f56b51618
jhairzp27 <jhairzp27@hotmail.com>
2026-05-29 17:32:57 -0500

    test: Agrego fase roja para caso 2 donde se espera 80 dolares

--- src/test/java/ec/edu/epn/skyroute/service/BaggageFeeCalculatorTest.java
+++ src/test/java/ec/edu/epn/skyroute/service/BaggageFeeCalculatorTest.java
@@ -34,4 +34,27 @@ class BaggageFeeCalculatorTest {
 //        ASSERT
         assertEquals(30.0, result, "El resultado no coincide con el esperado, La tarifa base debe ser de $30");
     }
+
+    @Test
+    @ExtendWith(MockitoExtension.class)
+    @DisplayName("Exceso de peso 1 maleta, 25 kg, pasajero regular, Cobra 80")
+    void deberiaCobrar80_CuandoHayExcesoDePesoYPasajeroRegular() {
+//        ARRANGE
+        Long passengerId = 1L;
+        double weight = 20.0;
+        int bagCount = 1;
+
+        when(passengerService.isVip(passengerId)).thenReturn(false);
+
+//        ACT
+        double result = calculator.calculateFee(weight, bagCount, passengerId);
+
+//        ASSERT
+//        30 (base) + 50 (exceso) = 80
+        assertEquals(80.0, result, "El costo total con exceso de peso debe ser $80.0");
+
+    }
+
+
+
 }
