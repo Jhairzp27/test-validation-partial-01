47e46bde56242c6a9a50ebdd0cd5563518824aac
jhairzp27 <jhairzp27@hotmail.com>
2026-05-29 17:24:49 -0500

    test: Agrego prueba roja para caso 1

---
+++ src/test/java/ec/edu/epn/skyroute/service/BaggageFeeCalculatorTest.java
@@ -0,0 +1,37 @@
+package ec.edu.epn.skyroute.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+class BaggageFeeCalculatorTest {
+    @Mock
+    private PassengerService passengerService;
+
+    @InjectMocks
+    private BaggageFeeCalculator calculator;
+
+    @Test
+    @ExtendWith(MockitoExtension.class)
+    @DisplayName("Debería cobrar $30.0 por una maleta estándar para un pasajero regular")
+    void deberiaCobrar30_WhenEsUnaMaletaEstandarYPasajeroRegular() {
+//        ARRANGE
+        Long passengerId = 1L;
+        double weight = 20.0;
+        int bagCount = 1;
+
+        when(passengerService.isVip(passengerId)).thenReturn(false);
+
+//        ACT
+        double result = calculator.calculateFee(weight, bagCount, passengerId);
+
+//        ASSERT
+        assertEquals(30.0, result, "El resultado no coincide con el esperado, La tarifa base debe ser de $30");
+    }
+}
